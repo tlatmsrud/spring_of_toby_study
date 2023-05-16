aa94dfb289ae0d13257436f69b21c3b45f7aec27
tlatmsrud <tlatmsrud@naver.com>
2023-05-16 18:21:48 +0900

    :recycle: jdbcContext 클래스 분리에 따른 코드 리팩토링

--- spring_study_week5/src/main/java/org/example/user/dao/UserDao.java
+++ spring_study_week5/src/main/java/org/example/user/dao/UserDao.java
@@ -1,7 +1,5 @@
 package org.example.user.dao;
 
-import org.example.user.dao.strategy.AddStatement;
-import org.example.user.dao.strategy.DeleteAllStatement;
 import org.example.user.dao.strategy.StatementStrategy;
 import org.example.user.domain.User;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -11,38 +9,44 @@ import java.sql.*;
 
 public class UserDao {
 
+    private JdbcContext jdbcContext;
+
     private DataSource dataSource;
+
+    public void setJdbcContext(JdbcContext jdbcContext){
+        this.jdbcContext = jdbcContext;
+    }
 
     public void setDataSource(DataSource dataSource){
         this.dataSource = dataSource;
     }
-
     public void add(User user) throws SQLException {
 
-        jdbcContextWithStatementStrategy(
-            new StatementStrategy(){
-                @Override
-                public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
-                    PreparedStatement ps = c.prepareStatement(
-                            "insert into users(id, name, password) values(?,?,?)");
+        jdbcContext.workWithStatementStrategy(
+                new StatementStrategy(){
+                    @Override
+                    public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
+                        PreparedStatement ps = c.prepareStatement(
+                                "insert into users(id, name, password) values(?,?,?)");
 
-                    ps.setString(1, user.getId());
-                    ps.setString(2, user.getName());
-                    ps.setString(3, user.getPassword());
-                    return ps;
-                }
-        });
+                        ps.setString(1, user.getId());
+                        ps.setString(2, user.getName());
+                        ps.setString(3, user.getPassword());
+                        return ps;
+                    }
+                });
+
     }
 
     public void deleteAll() throws SQLException{
-        jdbcContextWithStatementStrategy(new StatementStrategy() {
+        jdbcContext.workWithStatementStrategy(new StatementStrategy() {
             @Override
             public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
-                PreparedStatement ps = c.prepareStatement("delete from users");
-                return ps;
+                return c.prepareStatement("delete from users");
             }
         });
     }
+
 
     public User get(String id) throws SQLException{
         Connection c = null;
@@ -120,26 +124,4 @@ public class UserDao {
         return count;
     }
 
-    public void jdbcContextWithStatementStrategy(StatementStrategy stmt) throws SQLException{
-        Connection c = null;
-        PreparedStatement ps = null;
-
-        try{
-            c = dataSource.getConnection();
-
-            ps = stmt.makePreparedStatement(c);
-
-            ps.executeUpdate();
-
-            // execute : 수행 결과를 Boolean 타입으로 반환
-            // executeQuery : select 구문을 처리할 때 사용하며, 수행 결과를 ResultSet 타입으로 반환
-            // executeUpdate : INSERT, UPDATE, DELETE 구문을 처리할 때 사용하며, 반영된 레코드 수를 int 타입으로 반환.
-        }catch(SQLException e){
-            e.printStackTrace();
-            throw e;
-        }finally {
-            if(ps != null){ try{ ps.close();} catch (Exception e){e.printStackTrace();} }
-            if(c != null){ try{ c.close(); }catch(Exception e){ e.printStackTrace();} }
-        }
-    }
 }
