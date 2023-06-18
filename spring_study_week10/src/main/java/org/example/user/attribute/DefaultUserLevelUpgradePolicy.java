e5ee5fcf4808643d545c27650ef8d2af87f8534c
tlatmsrud <tlatmsrud@naver.com>
2023-06-18 15:41:47 +0900

    :sparkles: Add-mail-send

--- spring_study_week10/src/main/java/org/example/user/attribute/DefaultUserLevelUpgradePolicy.java
+++ spring_study_week10/src/main/java/org/example/user/attribute/DefaultUserLevelUpgradePolicy.java
@@ -4,7 +4,11 @@ import org.example.user.dao.IUserDao;
 import org.example.user.domain.User;
 import org.example.user.enums.Level;
 
-import javax.sql.DataSource;
+import javax.mail.*;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
 
 
 public class DefaultUserLevelUpgradePolicy implements UserLevelUpgradePolicy{
@@ -36,6 +40,43 @@ public class DefaultUserLevelUpgradePolicy implements UserLevelUpgradePolicy{
     public String upgradeLevel(User user){
         user.upgradeLevel();
         userDao.update(user);
+        sendUpgradeMail(user);
         return user.getId();
+    }
+
+    private void sendUpgradeMail(User user) {
+        Properties props = new Properties();
+        props.put("mail.host", "smtp.naver.com");
+        props.put("mail.port", "465");
+        props.put("mail.smtp.auth" , "true");
+        props.put("mail.smtp.ssl.enable", "true");
+        props.put("mail.smtp.ssl.trust", "smtp.naver.com");
+        props.put("mail.debug", "true");
+
+        Session s = Session.getInstance(props, new Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication("email","password");
+            }
+        });
+
+        MimeMessage message = new MimeMessage(s);
+
+        try{
+            message.setFrom(new InternetAddress("tlatmsrud@naver.com"));
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(user.getEmail()));
+            message.setSubject("Upgrade 안내");
+            message.setText("사용자님의 등급이 " + user.getLevel().name() +" 로 업그레이드 되었습니다.");
+            Transport.send(message);
+        } catch (AddressException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        } catch (MessagingException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+
     }
 }
