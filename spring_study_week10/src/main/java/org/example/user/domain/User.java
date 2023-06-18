01299ee703103bf1b72a286f234da493e85b7a13
tlatmsrud <tlatmsrud@naver.com>
2023-06-18 15:39:16 +0900

    :sparkles: Add-email-field

--- spring_study_week10/src/main/java/org/example/user/domain/User.java
+++ spring_study_week10/src/main/java/org/example/user/domain/User.java
@@ -10,14 +10,16 @@ public class User {
     private Level level;
     private int login;
     private int recommend;
+    private String email;
 
-    public User(String id, String name, String password, Level level, int login, int recommend){
+    public User(String id, String name, String password, Level level, int login, int recommend, String email){
         this.id = id;
         this.name = name;
         this.password = password;
         this.level = level;
         this.login = login;
         this.recommend = recommend;
+        this.email = email;
     }
 
     public User(){
@@ -77,5 +79,13 @@ public class User {
 
     public void setRecommend(int recommend) {
         this.recommend = recommend;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
