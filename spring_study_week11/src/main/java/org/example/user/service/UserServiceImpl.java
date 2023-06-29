fbb300f3bf5b8e847d623fd1b5fbcfa88ef88e5d
tlatmsrud <tlatmsrud@naver.com>
2023-06-29 21:17:02 +0900

    :fire: Remove-unnecessary-code

--- spring_study_week11/src/main/java/org/example/user/service/UserServiceImpl.java
+++ spring_study_week11/src/main/java/org/example/user/service/UserServiceImpl.java
@@ -4,20 +4,12 @@ import org.example.user.attribute.UserLevelUpgradePolicy;
 import org.example.user.dao.IUserDao;
 import org.example.user.domain.User;
 import org.example.user.enums.Level;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.TransactionStatus;
-import org.springframework.transaction.support.DefaultTransactionDefinition;
 import java.util.List;
 
-/**
- * title        :
- * author       : sim
- * date         : 2023-06-02
- * description  :
- */
 public class UserServiceImpl implements UserService {
 
     private IUserDao userDao;
+
 
     private UserLevelUpgradePolicy userLevelUpgradePolicy;
     public void setUserDao(IUserDao userDao){
