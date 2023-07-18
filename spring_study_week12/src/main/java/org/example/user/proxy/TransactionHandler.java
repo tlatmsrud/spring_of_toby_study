0c98b57a9a307bc59599438347a3656fd2370bcd
tlatmsrud <tlatmsrud@naver.com>
2023-07-18 19:52:59 +0900

    :pencil2: 주석추가

--- spring_study_week12/src/main/java/org/example/user/proxy/TransactionHandler.java
+++ spring_study_week12/src/main/java/org/example/user/proxy/TransactionHandler.java
@@ -9,10 +9,10 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 /**
- * title        :
+ * title        : 트랜잭션 부가기능
  * author       : sim
  * date         : 2023-07-05
- * description  :
+ * description  : 다이나믹 프록시에 사용할 트랜잭션 부가기능 클래스
  */
 public class TransactionHandler implements InvocationHandler {
     private Object target;
@@ -34,22 +34,29 @@ public class TransactionHandler implements InvocationHandler {
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
 
+        // 메서드 명이 pattern 으로 시작할 경우
         if(method.getName().startsWith(pattern)){
-            return invokeInTransaction(method, args);
+            return invokeInTransaction(method, args); // 트랜잭션 기능과 함께 메서드 실행
         }
-        return method.invoke(target, args);
+        return method.invoke(target, args); // 트랜잭션 기능 없이 메서드 실행
     }
 
     public Object invokeInTransaction(Method method, Object[] args) throws Throwable {
 
+        // 트랜잭션 생성
         TransactionStatus status = this.transactionManager
                 .getTransaction(new DefaultTransactionDefinition());
 
         try{
+            // 메서드 실행
             Object ret = method.invoke(target, args);
+
+            // 트랜잭션 commit
             this.transactionManager.commit(status);
             return ret;
         } catch (InvocationTargetException e) {
+
+            // 타겟 메서드 실행 중 예외 발생 시 트랜잭션 rollback
             this.transactionManager.rollback(status);
             throw e.getTargetException();
         }
